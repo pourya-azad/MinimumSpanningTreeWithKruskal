18cc334f9a3bdceb8f6e1d615c52ee4320568690
pouria <109802957+pouria-azad@users.noreply.github.com>
2025-06-28 09:57:31 +0330

    update

--- Models/Node.cs
+++ Models/Node.cs
@@ -9,7 +9,6 @@ namespace MinimumSpanningTreeWithKruskal.Models
 
         public required int GraphId { get; set; }
 
-        // Optional: برای رابطه دوطرفه با Edge
         public virtual ICollection<Edge> EdgesAsNode1 { get; set; } = new List<Edge>();
         public virtual ICollection<Edge> EdgesAsNode2 { get; set; } = new List<Edge>();
 
